5e5ccc822f5fc12b0b675e79cce2a51a4a439f62
Joel Roberts <52210416+joelroberts422@users.noreply.github.com>
2025-04-26 10:29:40 -0500

    front and back

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_folder='static/react',  # React build files
+            static_url_path='/app')
 
 @app.route('/')
-def index():
-    return jsonify({"message": "Hello, World!"})
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({"message": "Test endpoint!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
