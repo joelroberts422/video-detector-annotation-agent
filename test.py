7b75c01723913570c576c47d1c832e5ae31805c9
Joel Roberts <52210416+joelroberts422@users.noreply.github.com>
2025-04-26 13:39:18 -0500

    asldkfh

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-from detector import process_video
+from detector import process_video, json_to_paths
+import os
+import json
 
 
 VIDEO_FOLDER = 'videos'
+DATASETS_FOLDER = 'datasets'
 
 if __name__ == "__main__":
     video_name = "tapco.mp4"
 
-    process_video(video_name)
+    # process_video(video_name)
+
+    dataset_path = os.path.join(DATASETS_FOLDER, video_name.split('.')[0] + '.json')
+    paths = json_to_paths(dataset_path)
+
+    paths_file_path = os.path.join(DATASETS_FOLDER, video_name.split('.')[0] + '_paths.json')
+    with open(paths_file_path, 'w') as f:
+        json.dump(paths, f, indent=4)
+
